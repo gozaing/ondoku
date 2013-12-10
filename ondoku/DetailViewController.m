857e45d2e44cb6eb63b6ecb3ead32a0408710ec4
tobaruhideyasu <tobaru@tobaru-mac.local>
2013-12-10 23:16:00 +0900

    delete comment

--- ondoku/DetailViewController.m
+++ ondoku/DetailViewController.m
@@ -18,11 +18,6 @@
 @implementation DetailViewController
 {
 @private
-    //NSMutableArray *titleArry;
-    //NSMutableArray *contentsArry;
-    //NSMutableArray *textArry;
-    //NSMutableArray *idArry;
-    
     NSMutableArray *contentsArray;
     
 }
@@ -46,30 +41,15 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
-    //titleArry = [[NSMutableArray alloc] init ];
-    //textArry = [[NSMutableArray alloc] init];
-    //idArry  = [[NSMutableArray alloc] init];
     
     contentsArray  = [[NSMutableArray alloc] init];
-    
-    
-    //[DataModels selectTitle:titleArry]; //titleArryにtitleカラムのデータを格納する
-    //[DataModels selectText:textArry]; //textArryにtextfieldカラムのデータを格納
-    //[DataModels selectId:idArry]; //unique id column
     
     [DataModels selectAll:contentsArray];
     
     
     // 前のTableViewで選択した行に応じて表示させる（セットされたrow_numをここで使う）
-    //titleLabel.text = [titleArry objectAtIndex:row_num];
-    //textField.text = [textArry objectAtIndex:row_num];
-    
     titleLabel.text = [[contentsArray objectAtIndex:row_num] objectForKey:@"Title"];
     textField.text = [[contentsArray objectAtIndex:row_num] objectForKey:@"Text"];
-    
-    
-    
-    
     
     // 右上の「Save」ボタン。押したら、save_memo_photoメソッドが実行される
     UIBarButtonItem *button = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemEdit target:self action:@selector(mod_text)];
@@ -93,13 +73,7 @@
 
 - (IBAction)countUpDidPush:(UIButton *)sender {
     
-    NSLog(@"countUpDidPush->%d",sender.tag);
-//    [DataModels updateCount:(NSInteger)sender.tag Id:(NSInteger)[idArry objectAtIndex:row_num]];
-    
     [DataModels updateCount:(NSInteger)sender.tag Id:(NSInteger)[[contentsArray objectAtIndex:row_num] objectForKey:@"Id"]];
-
-    
-
 
 }
 
